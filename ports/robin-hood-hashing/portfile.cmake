8adcc9c8c43c05ff79f8678862efa777c091c0b2
ryan-rsm-mckenzie <ryan__mckenzie@hotmail.com>
2021-03-11 23:46:36 -0800

    cleanup cmake packages

--- ports/robin-hood-hashing/portfile.cmake
+++ ports/robin-hood-hashing/portfile.cmake
@@ -13,7 +13,10 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/robin_hood)
+vcpkg_cmake_config_fixup(
+	PACKAGE_NAME robin_hood
+	CONFIG_PATH lib/cmake/robin_hood
+)
 
 file(REMOVE_RECURSE
 	${CURRENT_PACKAGES_DIR}/debug
