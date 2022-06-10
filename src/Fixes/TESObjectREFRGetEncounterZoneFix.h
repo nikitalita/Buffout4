05367c2a5213979a100e759a1bc4ef3a9438b789
ryan-rsm-mckenzie <ryan__mckenzie@hotmail.com>
2022-06-09 19:00:32 -0700

    fix a crash caused by TESObjectREFRGetEncounterZone fix

--- src/Fixes/TESObjectREFRGetEncounterZoneFix.h
+++ src/Fixes/TESObjectREFRGetEncounterZoneFix.h
@@ -7,16 +7,19 @@ namespace TESObjectREFRGetEncounterZoneFix
 		template <class T>
 		struct GetEncounterZone
 		{
-			static T* thunk(const RE::TESObjectREFR& a_this)
+			static T* thunk(const RE::BSTSmartPointer<RE::ExtraDataList>& a_in)
 			{
-				auto ptr = a_this.extraList ? func(*a_this.extraList) : nullptr;
+				const auto& ref = *stl::adjust_pointer<RE::TESObjectREFR>(
+					&a_in,
+					-static_cast<std::ptrdiff_t>(offsetof(RE::TESObjectREFR, RE::TESObjectREFR::extraList)));
+				auto ptr = ref.extraList ? func(*ref.extraList) : nullptr;
 
 				const auto addr = reinterpret_cast<std::uintptr_t>(ptr);
-				if (!a_this.IsInitialized() &&
+				if (!ref.IsInitialized() &&
 					((addr & 0xFFFF'FFFF'0000'0000) == 0) &&
 					((addr & 0x0000'0000'FFFF'FFFF) != 0)) {
 					auto id = static_cast<std::uint32_t>(addr);
-					RE::TESForm::AddCompileIndex(id, a_this.GetFile());
+					RE::TESForm::AddCompileIndex(id, ref.GetFile());
 					ptr = RE::TESForm::GetFormByID<T>(id);
 				}
 
@@ -31,10 +34,10 @@ namespace TESObjectREFRGetEncounterZoneFix
 	{
 		const auto root = REL::ID(1413642).address();
 
-		REL::safe_fill(root + 0xD, REL::NOP, 0x7);
+		REL::safe_write(root + 0xD + 0x1, static_cast<std::uint8_t>(0x8D));  // mov -> lea
 		stl::write_thunk_call<5, detail::GetEncounterZone<RE::BGSEncounterZone>>(root + 0x14);
 
-		REL::safe_fill(root + 0x91, REL::NOP, 0x7);
+		REL::safe_write(root + 0x91 + 0x1, static_cast<std::uint8_t>(0x8D));  // mov -> lea
 		stl::write_thunk_call<5, detail::GetEncounterZone<RE::BGSLocation>>(root + 0x98);
 
 		logger::debug("installed TESObjectREFRGetEncounterZone fix"sv);
